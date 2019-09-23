7fefc73897e342082d4f4669eba6b25c267f7f2c
alenovan <novannanega@gmail.com>
2019-09-23 15:59:49 +0700

    tugas impilict explicit

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -8,11 +8,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
-
+    TextView text_output;
+    EditText input_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        text_output = findViewById(R.id.text_output);
+        input_name = findViewById(R.id.input_name);
     }
 
+    public void getDataText(View view) {
+        text_output.setText(input_name.getText());
+    }
 }
